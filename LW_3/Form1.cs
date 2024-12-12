97289c0a0d879ba3deb94d17a5ebd442acaea4b0
Doshtik <igor.lizyakinvladislavovic@gmail.com>
2024-12-12 10:55:19 +0500

    Добавьте файлы проекта.

---
+++ LW_3/Form1.cs
@@ -0,0 +1,10 @@
+namespace LW_3
+{
+    public partial class FormBinding : System.Windows.Forms.Form
+    {
+        public FormBinding()
+        {
+            InitializeComponent();
+        }
+    }
+}
